a272e440bcb23a29ebd635391a93a3f9547eda64
Gary McGrath <gary.mcgrath@gmail.com>
2024-04-03 21:41:57 +1300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-# The source path to backup. Can be local or remote.
-backup_source="/mnt/data/share/media"
+# Define our options
+backup_source="/mnt/data/share/media/"
+backup_target="/mnt/usb/rsync_backup/"
+global_opts="--archive --verbose --human-readable --delete --info=progress2,stats2"
+retention_points=3
 
-# Where to store the incremental backups
-backup_target="/mnt/usb/rsync_backup/file-srv"
+#########################################################################################
 
-# Where to store this backup
-current_backup="$backup_target/$(date +%Y-%m-%d_%H:%M:%S)"
+# Remove any items that have passed the retention period
 
-# Where to find the most recent backup
-last_backup=$(find "$backup_target" -mindepth 1 -maxdepth 1 -type d | sort | tail -n 1)
-
-# Define global options
-global_opts="--archive --verbose --human-readable --delete --info=progress2"
-
-# Define the number of backups to keep before starting to remove the oldest ones
-retention_points=10
-
+echo "Checking for backups past retention scope, if they are past scope they will be removed."
 # Get the number of directories in the backup target
 num_folders=$(ls -1 "$backup_target" | wc -l)
-
 # If there are more than the specified number of directories, remove the oldest ones
 if [ "$num_folders" -gt "$retention_points" ]; then
-    num_folders_to_remove=$((num_folders - retention_points))
-    for (( i=1; i<=num_folders_to_remove; i++ )); do
-        oldest_folder=$(ls -1 "$backup_target" | sort | head -n 1)
-        if [ -n "$oldest_folder" ]; then
-            echo "Removing $backup_target/$oldest_folder"
-            rm -rf "$backup_target/$oldest_folder"
-        fi
-    done
+        num_folders_to_remove=$((num_folders - retention_points))
+        for (( i=1; i<=num_folders_to_remove; i++ )); do
+                oldest_folder=$(find "$backup_target" -mindepth 1 -maxdepth 1 -type d | sort | head -n 1)
+                rm -rf "$oldest_folder"
+                echo "Removed: $oldest_folder"
+        done
 fi
 
+# Define backup job parameters
+current_backup="$backup_target$(date +%Y-%m-%d_%H:%M:%S)_full"
+last_backup=$(find "$backup_target" -mindepth 1 -maxdepth 1 -type d | sort | tail -n 1)
+
 # Use the most recent backup as the incremental base if it exists
-if [ -d "$last_backup" ]
-then
-        OPTS="$global_opts --link-dest $last_backup"
+if [ -d "$last_backup" ]; then
+        rename=$(find "$backup_target" -mindepth 1 -maxdepth 1 -type d | sort | tail -n 2 | head -n 1)
+        mv "$rename" "${rename//_full/}"
+        OPTS=""$global_opts" --link-dest "$last_backup""
+        echo "Performing incremental backup..."
 else
-        OPTS="$global_opts"
+        OPTS=""$global_opts" "--whole-file""
+        echo "Performing initial backup job, sending whole-files..."
 fi
 
 # Run the rsync
-rsync --delete $OPTS "$backup_source" "$current_backup"
+rsync $OPTS "$backup_source" "$current_backup"
